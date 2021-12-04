5b4e30cba41be739b974f5ab083de19aa3b4830c
tamhv055 <tamhv@live.com>
2021-12-05 06:27:14 +0700

    05/12/2021

--- Data/GetData.py
+++ Data/GetData.py
@@ -4,7 +4,6 @@ import config
 import sys
 sys.path.insert(1, "D:\project Binance") 
 sys.path.insert(1, "D:\project Binance\Data")
-
 import ExportTxt
 
 
@@ -16,34 +15,34 @@ else:
     print("connect success")
 
 
-def recent_price_ETH():
-    price = client.get_recent_trades(symbol='ETHUSDT',limit=1)
-    return price[0]["price"]
+def recent_price_ETH(_symbol):
+    price = client.get_recent_trades(symbol=_symbol,limit=1)
+    return float(price[0]["price"])
 
-def average_price_5mins_ETH():
-    avg_price = client.get_avg_price(symbol='ETHUSDT')
-    return avg_price['price']
+def average_price_5mins_ETH(_symbol):
+    avg_price = client.get_avg_price(symbol=_symbol)
+    return float(avg_price['price'])
 
 def get_balance(coinsymbol):
     balanceETH= client.get_asset_balance(asset=coinsymbol)
-    return balanceETH["free"]
+    return float(balanceETH["free"])
 
 def get_balance_ETH_Free():
     balanceETH= client.get_asset_balance(asset='ETH')
-    return balanceETH["free"]
+    return float(balanceETH["free"])
 
 def get_balance_USDT_Free():
     balanceUSDT= client.get_asset_balance(asset='USDT')   
-    return (balanceUSDT["free"])
+    return float((balanceUSDT["free"]))
 
 
 def get_fee_buy():
     feeETHUSDT = client.get_trade_fee(symbol='ETHUSDT')
-    return feeETHUSDT[0]['makerCommission']
+    return float(feeETHUSDT[0]['makerCommission'])
 
 def get_fee_sell():
     feeETHUSDT = client.get_trade_fee(symbol='ETHUSDT')
-    return feeETHUSDT[0]['takerCommission']
+    return float(feeETHUSDT[0]['takerCommission'])
 
 
 def get_candles(_symbol,KlineTime):
@@ -55,7 +54,7 @@ def get_open_price(_symbol,KlineTime,count):
     candles = client.get_klines(symbol=_symbol, interval=KlineTime)
     #candles have 500 element, but we have get "Count" element with Candles[count:]
     #print(candles[count:])
-    for x in candles[count:]:
+    for x in candles[(0-count):]:
         #print(x)
         #print(x[1])
         list_open_price.append(float(x[1]))
@@ -64,7 +63,7 @@ def get_open_price(_symbol,KlineTime,count):
 def get_close_price(_symbol,KlineTime,count):
     list_close_price=[]
     candles = client.get_klines(symbol=_symbol, interval=KlineTime) 
-    for x in candles[count:]:
+    for x in candles[(0-count):]:
         list_close_price.append(float(x[4]))
 
     return list_close_price
@@ -72,31 +71,34 @@ def get_close_price(_symbol,KlineTime,count):
 def get_high_price(_symbol,KlineTime,count):
     list_high_price=[]
     candles = client.get_klines(symbol=_symbol, interval=KlineTime) 
-    for x in candles[count:]:
+    for x in candles[(0-count):]:
         list_high_price.append(float(x[2]))
 
-    return list_high_price
+    return float(list_high_price)
 
 def get_low_price(_symbol,KlineTime,count):
     list_low_price=[]
     candles = client.get_klines(symbol=_symbol, interval=KlineTime) 
-    for x in candles[count:]:
+    for x in candles[(0-count):]:
         list_low_price.append(float(x[3]))
 
-    return list_low_price
+    return float(list_low_price)
 
 def Calculator_Average_Listprice(list_price:list):
     len_list  = len(list_price)
     sum_price = sum(list_price)
-    return sum_price/len_list
+    return float(sum_price/len_list)
 
 def CalCulator_safepoint(_symbol,KlineTime,count):
     list_low = get_low_price(_symbol,KlineTime,count)
-    Low_Average = Calculator_Average_Listprice(list_low)
-    list_high = get_high_price(_symbol,KlineTime,count)
-    High_Average = Calculator_Average_Listprice(list_low)
     
-    return (Low_Average+High_Average)/2
+    Low_Average = Calculator_Average_Listprice(list_low)
+    
+    list_high = get_high_price(_symbol,KlineTime,count)
+    
+    High_Average = Calculator_Average_Listprice(list_high)
+    
+    return float((Low_Average+High_Average)/2)
 
 
 """ print(recent_price_ETH())
